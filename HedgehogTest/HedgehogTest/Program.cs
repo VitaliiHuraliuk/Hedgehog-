a6c6bf10395384a83a4bb4527f6bc48a0a6272c8
VitaliiHuraliuk <116571638+VitaliiHuraliuk@users.noreply.github.com>
2024-03-28 15:55:45 +0200

    Solution of the task

---
+++ HedgehogTest/HedgehogTest/Program.cs
@@ -0,0 +1,23 @@
+﻿class MainClass
+{
+    public static int ChangeColor(int[] population, int desiredColor)
+    {
+        // Перевірка, чи всі їжачки вже мають бажаний колір
+        if (population[desiredColor] == population[0] + population[1] + population[2])
+            return 0;
+
+        if (population[desiredColor] == 0)
+            return -1;
+
+        return (Math.Max(population[0], Math.Max(population[1], population[2])) - population[desiredColor] + 1) / 2;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] population = new int[] { 8, 1, 9 };
+        int desiredColor = 1; // Бажаний колір: зелений
+
+        int result = ChangeColor(population, desiredColor);
+        Console.WriteLine("Мінімальна кількість зустрічей, необхідних для зміни всіх їжачків у зелений колір: " + result);
+    }
+}
